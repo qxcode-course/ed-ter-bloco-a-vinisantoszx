fabcee06ff42565d87adb777c72a2d6426524de8
Vinícius Santos <115155877+vinisantoszx@users.noreply.github.com>
2026-03-31 02:43:02 +0000

    feat: atvd apagando

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -1,4 +1,32 @@
 package main
+
 import "fmt"
+
+// ANOTAÇÃO PESSOAL - O SCANF NÃO FUNCIONOU NESSE CASO, TIRAR DÚVIDA COM O PROFESSOR NA AULA DE ED
 func main() {
+	var n, m, id int
+
+	fmt.Scan(&n)
+
+	fila := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&fila[i])
+	}
+
+	fmt.Scan(&m)
+
+	desistiu := make(map[int]bool)
+
+	for i := 0; i < m; i++ {
+		fmt.Scan(&id)
+		desistiu[id] = true
+	}
+
+	for _, pessoa := range fila {
+		if !desistiu[pessoa] {
+			fmt.Printf("%d ", pessoa)
+		}
+	}
+
+	fmt.Println()
 }
